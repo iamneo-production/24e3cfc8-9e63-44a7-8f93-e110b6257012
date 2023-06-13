798f62358f8f6be1b0646f88cfaaa4c5e36e0628
yuvrajghule <yuvrajghule281@gmail.com>
2023-06-13 11:52:49 +0000

    added login page

--- reactapp/src/Components/Loginpage.js
+++ reactapp/src/Components/Loginpage.js
@@ -1,42 +1,12 @@
 import { useState, useEffect } from "react";
 import "../Styles/login.css";
-// import axios from 'axios';
-// import { useNavigate, Link } from "react-router-dom";
+
 export default function LoginPage() {
-    // const navigate = useNavigate();
     const [id, setid] = useState("")
     const [pass, setpass] = useState("")
     const [credential, setCredential] = useState([]);
 
-    // useEffect(() => {
-    //     // Fetching user credentials from API
-    //     axios.get('http://localhost:8080/api/getcred')
-    //         .then(response => setCredential(response.data))
-    //         .catch(error => console.error(error));
-    // }, []);
 
-
-    // function handleLogin() {
-    //     var l=true;
-    //     credential.map((cred) =>{
-    //         if (cred.email === id && cred.password === pass) {
-    //             l=true
-
-    //         }
-    //     });
-
-    //     if (l) {
-    //         props.changeLog()
-    //         navigate("/home")
-    //         console.log("logged in")
-    //     }
-    //     else {
-    //         console.log("logged out")
-    //         console.log(credential.email)
-
-
-    //     }
-    // }
     return (
         <>
             <div className="header">Login</div>
