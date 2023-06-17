32d4bb112b5ec7f8cd552235342c64acff876507
Saikalyan19 <saikalyanyadav19@gmail.com>
2023-06-17 10:55:39 +0000

    Login Commit

--- reactapp/src/Components/Loginpage.js
+++ reactapp/src/Components/Loginpage.js
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
-import "../Styles/login.css";
+import "./login.css";
+export default function LoginPage(props) {
+    const [data, setData] = useState({
+        email:"",
+        password:""
+    });
 
-export default function LoginPage() {
-    const [id, setid] = useState("")
-    const [pass, setpass] = useState("")
-    const [credential, setCredential] = useState([]);
-
-
+    const handleLogin =()=>
+    {
+        alert(clicked);
+    }
+    
     return (
         <>
             <div className="header">Login</div>
             <div id="loginBox">
-                <input  className="input" id="email" placeholder="Enter email"></input><br />
-                <input type="password" className="input" id="password" placeholder="Enter password"></input>
-                <div><button id="loginButton" >Login</button></div>
+                <input onChange={(e) => setData({...data,email:e.target.value})} className="input" id="email" placeholder="Enter email"></input><br />
+                <input onChange={(e) => setData({...data,password:e.target.value})} type="password" className="input" id="password" placeholder="Enter password"></input>
+                <div><button id="loginButton" onClick={handleLogin}>Login</button></div>
                 <div>New user? Sign Up</div>
 
             </div>
