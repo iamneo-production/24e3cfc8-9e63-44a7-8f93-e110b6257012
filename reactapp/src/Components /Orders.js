b485f7834376d1a091ccc3cc9148adbcb918e74c
yoganithi <nithiyoga6@gmail.com>
2023-06-26 15:09:22 +0000

    Orders page

---
+++ reactapp/src/Components /Orders.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import "./home.css";
+import axios from "axios";
+import { getcurrentuser } from "./auth/authentication";
+export default function Orders({order,setcart}){
+ const [orders,setOrders] =useState([]);
+  const user=getcurrentuser();
+  const [paymentStatus, setPaymentStatus] = useState(null);
+
+  const handlePay = async (e) => {
+    e.preventDefault();
+    setPaymentStatus("Payment successful");
+  }
+
+  useEffect(()=>
+  {
+    axios.get('http://localhost:8080/orders',{
+      params: {
+        userId: user.email
+    }
+  }
+  ).then(response=>
+      {
+        setOrders(response.data)
+        console.log(orders);
+      }).catch(error=>console.log(error))
+  },[])
+    return(
+        <>
+        <NavBar />
+        { order.length ?
+        <div className="frame1">
+        <div className="cartGrid">
+          
+        <div className="cartTable">
+                    <table>
+                    <thead >
+                      <tr>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {order.map((item, index) => (
+                        <tr key={index}>
+                          <td>{index + 1}.&nbsp;&nbsp;{item.productName}</td>
+                          <td>{item.price}</td>
+                          <td>{item.quantity}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  </div>
+                  {paymentStatus ? (
+                <p style={{ fontWeight: 'bold', color: 'darkblue', textAlign: 'center' }}>{paymentStatus}</p>
+              ) : 
+                  (<button onClick={handlePay} className="placeButton">Pay</button>)}
+                  </div>
+                  </div> : <h1>No any order placed yet</h1> }
+                  
+
+        </>
+    )
+}
