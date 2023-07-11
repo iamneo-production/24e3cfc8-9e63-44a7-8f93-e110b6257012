9566d582ac0fd91d58c926d9e28fa7e7f839cd8c
hemanth-2 <hemanthreddybattula945@gmail.com>
2023-07-11 10:57:54 +0000

    order button

---
+++ reactapp/src/Components/OrderButton.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import "../Styles/home.css";
+
+export default function OrderButton ({ prod ,userId ,productQuantity}){
+    const orderHandler=()=>{
+        const item={
+          productName:prod.productName,
+          quantity:productQuantity,
+          price:prod.price
+        };
+        
+        const newOrder = {
+          userId: userId,
+          orderItems: [item],
+          status:"In Progress"
+        };
+        
+        console.log(newOrder);
+        
+        try {
+                axios.post('http://localhost:8080/saveOrders', newOrder);
+                } catch (error) {
+                  console.error(error);
+                }
+          }
+        return(
+            <button id="cartbutton" className="BL" onClick={orderHandler}>
+      place order
+    </button>
+        );
+}
