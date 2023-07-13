43f784fb69d3cd4c673e1c2f88eff8de9fc42e42
Saikalyan19 <saikalyanyadav19@gmail.com>
2023-07-13 07:50:36 +0000

    samp

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,55 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
-import LoginPage from "./Components/LoginPage"
-import PrivatRoute from "./Components/PrivatRoute"
-import Product from "./Components/Product"
-import Home from "./Components/Home"
-import Cart from "./Components/Cart"
-import Orders from "./Components/Orders"
-import RegisterPage from "./Components/RegisterPage"
-import Manageorders from './Components/Manageorders';
-import Manageproducts from './Components/Manageproducts';
-import {BrowserRouter,Routes, Route} from "react-router-dom";
-import NavBar from './Components/NavBar';
-import { useState } from 'react';
-import { doLogout } from './Components/auth/authentication';
-
+import LoginPage from './Components/Login';
 
 function App() {
-  const [Auth, setAuth]=useState(false);
-  const [cart, setcart]=useState([]);
-  const [order, setorder]=useState([]);
-
-  const changeLog=()=>{
-    setAuth(true)
-  }
-  const Logout=()=>{
-    setAuth(false)
-    doLogout();
-  }
-  
   return (
-    <BrowserRouter>
-      <Routes>
-        
-        <Route element={<PrivatRoute Auth={Auth}/>} >
-        <Route path="home" element={<Home Logout={Logout} cart={cart} setcart={setcart}/>} />
-        <Route path="cart" element={<Cart cart={cart} order={order} setorder={setorder} setcart={setcart}/>} />
-        <Route path="orders" element={<Orders  order={order} setcart={setcart} />} />
-        <Route path="manageorders" element={<Manageorders/>} />
-        <Route path="manageproducts" element={<Manageproducts/>} />
-      </Route>
-
-      {/* <Route element={<PrivatRouteForAdmin Auth={Auth}/>} >
-        <Route path="home" element={<AHome Logout={Logout} cart={cart} setcart={setcart}/>} />
-        <Route path="cart" element={<Aorders cart={cart} order={order} setorder={setorder} setcart={setcart}/>} />
-        <Route path="orders" element={<Orders  order={order} setcart={setcart} />} />
-      </Route> */}
-       
-        <Route path="/" element={<LoginPage Auth={Auth} changeLog={changeLog}/>}/>
-        <Route path="register" element={<RegisterPage />} />
-      </Routes>
-      </BrowserRouter>
-   
+    <div className="App">
+      <LoginPage/>
+    </div>
   );
 }
 
