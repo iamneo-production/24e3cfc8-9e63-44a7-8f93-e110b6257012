892c81da4df0225ff80a544461cadc50463c72c7
RajmaneeshNisangi <63787286+RajmaneeshNisangi@users.noreply.github.com>
2023-07-24 18:32:31 +0530

    Update Orderitems.java

--- springapp/src/main/java/com/example/springapp/model/Orderitems.java
+++ springapp/src/main/java/com/example/springapp/model/Orderitems.java
@@ -1,70 +1,49 @@
-import { useEffect, useState } from "react";
-import NavBar from "./NavBar";
-import "./home.css";
-import axios from "axios";
-import { getcurrentuser } from "./auth/authentication";
-import { useLocation, useParams } from 'react-router-dom';
-import Button from '@mui/material/Button';
-export default function OrderItems(){
-//  const [order,setOrder] =useState([]);
-  const user=getcurrentuser();
-  const [paymentStatus, setPaymentStatus] = useState(null);
-  const { orderId } = useParams();
-  const [orderItems, setOrderItems] = useState([]);
-  const handlePay = async (e) => {
-    e.preventDefault();
-    setPaymentStatus("Payment successful");
-  }
-  useEffect(() => {
-    fetchOrderItems(orderId);
-  }, [orderId]);
-  const fetchOrderItems = async (orderId) => {
-    try {
-      const response = await axios.get(`http://localhost:8080/orders/${orderId}`);
-      const data = response.data;
-      setOrderItems(data.orderItems);
-      console.log(orderItems);
-    } catch (error) {
-      console.log('Error fetching order items:', error);
-    }
-  };
-  console.log(orderId);
-    return(
-        <>
-        <NavBar />
-        { true ?
-        <div className="frame1">
-        <div className="cartGrid">
-          
-        <div className="cartTable">
-                    <table>
-                    <thead >
-                      <tr>
-                        <th>Productname</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                      </tr>
-                    </thead>
-                          <tbody className="outerRow" >
-                        {orderItems.map((product)=>(
-                        <tr>
-                          <td>{product.productName}</td>
-                          <td>{`Rs ${product.price}`}</td>
-                          <td>{product.quantity}</td>
-                        </tr>
-                        
-                      ))}
-                    </tbody>
-                  </table>
-                  </div>
-                  {paymentStatus ? (
-                <p style={{ fontWeight: 'bold', color: 'darkblue', textAlign: 'center' }}>{paymentStatus}</p>
-              ) : 
-                  (<Button onClick={handlePay} variant="contained" className="placeButton">Pay</Button>)}
-                  </div>
-                  </div> : <h1>No any order placed yet</h1> }
-                  
+package com.examly.springapp.model;
+import javax.persistence.*;
 
-        </>
-    )
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+@Entity
+@Table(name="OrderItems")
+public class OrderItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long orderItemsId;
+	private String productName;
+	private int quantity;
+	private String price;
+	public OrderItems() {
+		
+	}
+	public OrderItems(long orderItemsId, String productName, int quantity, String price) {
+		this.orderItemsId = orderItemsId;
+		this.productName = productName;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	public long getOrderItemsId() {
+		return orderItemsId;
+	}
+	public void setOrderItemsId(long orderItemsId) {
+		this.orderItemsId = orderItemsId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
 }
