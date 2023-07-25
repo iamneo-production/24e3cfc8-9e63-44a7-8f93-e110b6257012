37ae0e8d687bc4694f25bffddaadb91943f41049
yoganithi <nithiyoga6@gmail.com>
2023-07-25 14:28:00 +0000

    complete code

---
+++ reactapp/src/Components/Home.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import "../styles/home.css";
+import axios from 'axios';
+import { isLoggedIn } from "./auth/authentication";
+import Product from "./Product";
+
+export default function Home({ Logout, cart, setcart }) {
+    let data = JSON.parse(localStorage.getItem("data"));
+    const [id, setid] = useState(data.email)
+    const [product, setproduct] = useState([])
+    const [user, setUser] = useState(true);
+    useEffect(() => {
+        // Fetch getproductproduct details from the API
+        axios.get("https://8080-ecdecfbaaafafbbcefaabfececdfccc.project.examly.io/home").then(response => {
+            setproduct(response.data);
+            setUser(isLoggedIn());
+            console.log(response.data);
+        }).catch(error=>console.log(error))
+        
+    }, []);
+
+    if (!product) {
+        return <div>Loading</div>
+    }
+    return (
+        <>
+            <NavBar Logout={Logout} />
+            <div id="productHomeBody" className="homePage">
+                <h1 style={{fontFamily: 'Gill Sans' }}>Welcome</h1>
+                {user ? product.map((prod, idd) => (
+                    <Product prod={prod} cart={cart} setcart={setcart} key={idd} id={id} />
+                )) : <h1>Admin</h1>}
+            </div>
+
+        </>
+    )
+}
---
+++ reactapp/src/Components/OrderButton.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import "../styles/home.css";
+
+export default function OrderButton ({ prod ,userId ,productQuantity}){
+    const orderHandler=()=>{
+        const item={
+          productName:prod.productName,
+          quantity:productQuantity,
+          price:prod.price
+        };
+        
+        const newOrder = {
+          userId: userId,
+          orderItems: [item],
+          status:"In Progress"
+        };
+        
+        console.log(newOrder);
+        
+        try {
+                axios.post('https://8080-ecdecfbaaafafbbcefaabfececdfccc.project.examly.io/saveOrders', newOrder);
+                } catch (error) {
+                  console.error(error);
+                }
+          }
+        return(
+            <button id="cartbutton" className="BL" onClick={orderHandler}>
+      place order
+    </button>
+        );
+}
