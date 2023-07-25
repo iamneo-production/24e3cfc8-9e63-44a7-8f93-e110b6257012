5b90e6ea928978189b5636816458e9ea7938883c
kavya22439 <108606256+kavya22439@users.noreply.github.com>
2023-07-25 10:29:37 +0530

    Update Home.js

--- reactapp/src/Components/Home.js
+++ reactapp/src/Components/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import NavBar from "./NavBar";
-import "../Styles/home.css";
+import "../styles/home.css";
 import axios from 'axios';
 import { isLoggedIn } from "./auth/authentication";
 import Product from "./Product";
@@ -8,16 +8,16 @@ import Product from "./Product";
 export default function Home({ Logout, cart, setcart }) {
     let data = JSON.parse(localStorage.getItem("data"));
     const [id, setid] = useState(data.email)
-    // const [cart, setcart]=useState([]);
     const [product, setproduct] = useState([])
-    const [user, setUser] = useState(false);
+    const [user, setUser] = useState(true);
     useEffect(() => {
         // Fetch getproductproduct details from the API
-        axios.get("http://localhost:8080/home").then(response => {
+        axios.get("https://8080-ecdecfbaaafafbbcefaabfececdfccc.project.examly.io/home").then(response => {
             setproduct(response.data);
+            setUser(isLoggedIn());
             console.log(response.data);
-        })
-        setUser(isLoggedIn());
+        }).catch(error=>console.log(error))
+        
     }, []);
 
     if (!product) {
@@ -27,7 +27,7 @@ export default function Home({ Logout, cart, setcart }) {
         <>
             <NavBar Logout={Logout} />
             <div id="productHomeBody" className="homePage">
-                <h1 style={{ color: "white", fontFamily: 'Gill Sans' }}>Welcome</h1>
+                <h1 style={{fontFamily: 'Gill Sans' }}>Welcome</h1>
                 {user ? product.map((prod, idd) => (
                     <Product prod={prod} cart={cart} setcart={setcart} key={idd} id={id} />
                 )) : <h1>Admin</h1>}
