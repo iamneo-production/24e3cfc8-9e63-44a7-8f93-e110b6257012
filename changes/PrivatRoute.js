12bfcffbdfe9e7bb95a3dbd51e7465130c18a0b5
sakshigadhave78 <sakshigadhave78@gmail.com>
2023-07-31 08:55:31 +0000

    commit

---
+++ changes/PrivatRoute.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivatRoute = (props) => {
+  // let auth={'token': false}
+  return (
+   props.Auth ? <Outlet /> : <Navigate to="/" />
+  )
+}
+
+export default PrivatRoute
